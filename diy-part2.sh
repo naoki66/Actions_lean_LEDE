15809d0fac2b38dfa7339dfd6f84cfed060739e5
naoki66 <naoki66@163.com>
2022-09-18 15:37:05 +0800

    smartdns: bump to 1.2022.38

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,8 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/applic
 
 
 #升级smartdns版本
-#sed -i 's/2018-2020/2018-2022/g' feeds/packages/net/smartdns/Makefile
-#sed -i 's/1.2021.35/1.2022.38/g' feeds/packages/net/smartdns/Makefile
-#sed -i 's/f50e4dd0813da9300580f7188e44ed72a27ae79c/1991a0b102e891f149647b162897bf4403f8f66c/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/2018-2020/2018-2022/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/1.2021.35/1.2022.38/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/5a2559f0648198c290bb8839b9f6a0adab8ebcdc/1991a0b102e891f149647b162897bf4403f8f66c/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/29f0aa9eea2f98765be5377266c4b0c51a089ec132d51cd07a6adaec157f3b4b/8017fd769d8128af5b54ce7935f4801cc798c6608dd54fa3e3a7d230ec8f1b64/' feeds/packages/net/smartdns/Makefile
 #sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
