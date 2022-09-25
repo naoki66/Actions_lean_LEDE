3c7befadf49996e3eda21ea58a944a99a45c5764
yuayu <naoki66@163.com>
2022-09-25 22:00:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP 修改默认IP
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 恢复首页显示
@@ -18,6 +18,8 @@ sed -i 's/^#\(.*mwan.htm\)/\1/' package/lean/default-settings/files/zzz-default-
 sed -i 's/^#\(.*upnp.htm\)/\1/' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/^#\(.*ddns.htm\)/\1/' package/lean/default-settings/files/zzz-default-settings
 
+# 日期
+sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' package/lean/autocore/files/x86/index.htm
 
 # 把密码改成空
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
@@ -26,6 +28,10 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 #添加额外软件包
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/applications/luci-app-smartdns
 
+# 恢复主机型号
+sed -i 's/(dmesg | grep .*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
+sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 
 
 #升级smartdns版本
