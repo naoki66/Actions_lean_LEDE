25abc2844f697945ba4f0dca4bc4e7fad7d1d11d
yuayu <naoki66@163.com>
2022-09-28 12:49:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,9 +34,8 @@ sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
 sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 
 
-#升级smartdns版本
+#升级smartdns版本到最新Commits on Sep 28, 2022
 #sed -i 's/2018-2020/2018-2022/g' feeds/packages/net/smartdns/Makefile
-#sed -i 's/1.2021.35/1.2022.38/g' feeds/packages/net/smartdns/Makefile
-#sed -i 's/5a2559f0648198c290bb8839b9f6a0adab8ebcdc/1991a0b102e891f149647b162897bf4403f8f66c/g' feeds/packages/net/smartdns/Makefile
-#sed -i 's/29f0aa9eea2f98765be5377266c4b0c51a089ec132d51cd07a6adaec157f3b4b/8017fd769d8128af5b54ce7935f4801cc798c6608dd54fa3e3a7d230ec8f1b64/' feeds/packages/net/smartdns/Makefile
-#sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
+#sed -i 's/1.2022.38/1.2022.38/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/1991a0b102e891f149647b162897bf4403f8f66c/a13433abac2b725143ad8cd1586f21faa8b59e55/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
