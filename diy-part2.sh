f742c2a52ebc128e87dec02261f1138c071d1373
yuayu <naoki66@163.com>
2022-11-05 23:23:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,7 +35,7 @@ sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
 sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 
 
-#升级smartdns版本到最新Commits on Oct 23, 2022
-sed -i 's/1.2022.38/2022.10.23/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/1991a0b102e891f149647b162897bf4403f8f66c/391ef310b47cdd93989a4d6d3a7f9024537739d9/g' feeds/packages/net/smartdns/Makefile
+#升级smartdns版本到最新2022/11/05
+sed -i 's/1.2022.38/2022.11.05/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/1991a0b102e891f149647b162897bf4403f8f66c/5558e38cb4d2db55466f40ba327027409d9ed26e/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
