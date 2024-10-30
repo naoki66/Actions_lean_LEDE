08c936b365c430079c4dd913d2d52e1ce45145a3
yuayu <naoki66@163.com>
2024-10-30 23:59:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source 添加额外软件包
-echo 'src-git helloworld  https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
-echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
+#echo 'src-git helloworld  https://github.com/fw876/helloworld.git;main' >>feeds.conf.default
+#echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >> feeds.conf.default
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
